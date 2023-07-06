d190b318eb19479e47735ab9ad1791ba1d01b2b6
Aleksandrs <shalunovaaa@yandex.ru>
2023-07-06 15:56:19 +0300

    Task3

---
+++ task3/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int minValue = Prompt("Введите число для минимального диапазона значений: ");
+int maxValue = Prompt("Введите число для максимального диапазона значений: ");
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+double result = Average(array);
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    Random random = new Random();
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = random.Next(min, max);
+        }
+    }
+    return array;
+}
+
+double Average(int[,] array)
+{
+    double[] averages = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        double average = sum / array.GetLength(0);
+        averages[j] = average;
+        Console.WriteLine($"Среднее арифметическое для столбца {j + 1}: {average}");
+    }
+    return averages.Average(); 
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
